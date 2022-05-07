1d42d6d94b0ac8c0c60a575449b2f4ede5b09220
Karvendhan M <karvsmech@gmail.com>
2022-05-07 13:08:45 +0530

    function to classify tweets using naive bayes

--- NaiveBayesClassification/utils.py
+++ NaiveBayesClassification/utils.py
@@ -1,14 +1,14 @@
-import nltk
-from nltk.corpus import twitter_samples
-import random
-import matplotlib.pyplot as plt
+import numpy as np
 import re  # library for regular expression operations
 import string  # for string operations
 
 from collections import defaultdict
+
+import pandas as pd
 from nltk.corpus import stopwords  # module for stop words that come with NLTK
 from nltk.stem import PorterStemmer  # module for stemming
 from nltk.tokenize import TweetTokenizer  # module for tokenizing strings
+
 
 def process_tweet(tweet):
     '''
@@ -43,6 +43,7 @@ def process_tweet(tweet):
 
     return tweets_clean
 
+
 def build_freqs(tweets, labels):
     '''
 
@@ -60,10 +61,36 @@ def build_freqs(tweets, labels):
     return freq_dict
 
 
+def get_naive_bayes_score(tweets, cond_prob_dict, n_pos_tweets, n_neg_tweets,
+                       str1='pos_conditional_prob', str2='neg_conditional_prob'):
+    '''
+    calcualtes Naive Bayes classification for all input tweets. 1 is positive and 0 is negative
+    :param tweets:
+    :param cond_prob_dict:
+    :param n_pos_tweets:
+    :param n_neg_tweets:
+    :param str1:
+    :param str2:
+    :return:
+    '''
 
+    lst = []
+    prior_prob = np.log((n_pos_tweets/n_neg_tweets))
+    for tweet in tweets:
+        lambda_ = 0.0
+        for word in process_tweet(tweet):
 
+            pos_cond_prob = cond_prob_dict.get(word, 0.0000001)
+            if isinstance(pos_cond_prob, dict):
+                pos_cond_prob = pos_cond_prob.get(str1, 0.0000001)
+            neg_cond_prob = cond_prob_dict.get(word, 0.0000001)
+            if isinstance(neg_cond_prob, dict):
+                neg_cond_prob = neg_cond_prob.get(str2, 0.0000001)
+            lambda_ += np.log((pos_cond_prob/neg_cond_prob))
 
+        NBscore = lambda_ + prior_prob
+        NBscore = float(1.0 if NBscore >= 1.0 else 0.0)
+        lst.append(NBscore)
 
-
-
+    return lst
 
