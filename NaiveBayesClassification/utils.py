b342e28f74c6f62bb8bc9446ddda7cd96b287b8f
Karvendhan M <karvsmech@gmail.com>
2022-05-09 09:44:48 +0530

    adding a function to plot confidence ellipse of a bivariate

--- NaiveBayesClassification/utils.py
+++ NaiveBayesClassification/utils.py
@@ -1,10 +1,9 @@
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
 import numpy as np
 import re  # library for regular expression operations
 import string  # for string operations
 
-from collections import defaultdict
-
-import pandas as pd
 from nltk.corpus import stopwords  # module for stop words that come with NLTK
 from nltk.stem import PorterStemmer  # module for stemming
 from nltk.tokenize import TweetTokenizer  # module for tokenizing strings
@@ -93,4 +92,60 @@ def get_naive_bayes_score(tweets, cond_prob_dict, n_pos_tweets, n_neg_tweets,
         lst.append(NBscore)
 
     return lst
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of `x` and `y`
+
+    Parameters
+    ----------
+    x, y : array_like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensional dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
 
