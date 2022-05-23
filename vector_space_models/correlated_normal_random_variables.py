915129dcee0a99aff5ebad7199008421874450a2
Karvendhan M <karvsmech@gmail.com>
2022-05-23 09:53:51 +0530

    coorelated normal random variables

---
+++ vector_space_models/correlated_normal_random_variables.py
@@ -0,0 +1,45 @@
+import numpy as np                         # Linear algebra library
+import matplotlib.pyplot as plt            # library for visualization
+from sklearn.decomposition import PCA      # PCA library
+import pandas as pd                        # Data frame library
+import math                                # Library for math functions
+import random
+
+from scipy.stats import pearsonr
+
+np.random.seed(100)
+
+std1 = 1
+std2 = 0.333
+
+x = np.random.normal(0, std1, 1000)
+y = np.random.normal(0, std2, 1000)
+
+row_before_centering = pearsonr(x, y)
+
+# centering the data
+x = x - np.mean(x)
+y = y - np.mean(y)
+
+row_after_centering = pearsonr(x, y)
+
+n = 1
+angle = np.arctan(1/n)
+angle_in_degrees = (angle * 180)/math.pi
+
+rotationMatrix = np.array([
+    [np.cos(angle), np.sin(angle)],
+    [-np.sin(angle), np.cos(angle)]
+])
+
+xy = np.concatenate(([x] , [y]), axis=0).T
+data = np.dot(xy, rotationMatrix)
+
+data = pd.DataFrame(data, columns=['x', 'y'])
+row_after_rotation = pearsonr(data.x, data.y)
+
+plt.scatter(data.x, data.y)
+plt.show()
+
+
+
