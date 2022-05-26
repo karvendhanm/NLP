5cd772eac8dd8a18eab4d06e57745986743ac795
Karvendhan M <karvsmech@gmail.com>
2022-05-26 16:18:07 +0530

    week 3 assignment identifying countries from capitals

--- vector_space_models/utils.py
+++ vector_space_models/utils.py
@@ -1,5 +1,9 @@
 import numpy as np
+from operator import itemgetter
+import pickle
 
+fh = open('./data/Files/word_embeddings_subset.p', 'rb')
+word_embeddings = pickle.load(fh)
 
 def euclidean_distance(vector1, vector2):
     '''
@@ -21,5 +25,63 @@ def cosine_similarity(vector1, vector2):
     numerator = np.dot(vector1, vector2)
     denominator = np.linalg.norm(vector1) * np.linalg.norm(vector2)
     return numerator / denominator
+
+def get_word_vector(embeddings, word):
+    '''
+
+    :param embeddings:
+    :param word:
+    :return:
+    '''
+    return embeddings[word]
+
+def get_closest_vector(embeddings, vector, *args):
+    '''
+
+    :param embeddings:
+    :param vector:
+    :param args:
+    :return:
+    '''
+
+    similarity = -1
+    country = ''
+    for key in embeddings.keys():
+        if key not in args:
+            score = cosine_similarity(embeddings[key], vector)
+
+            if score > similarity:
+                similarity = score
+                country = key
+
+    return country
+
+
+def get_country(city1, country1, city2, embeddings, cosine_similarity=cosine_similarity):
+    """
+    Input:
+        city1: a string (the capital city of country1)
+        country1: a string (the country of capital1)
+        city2: a string (the capital city of country2)
+        # CODE REVIEW COMMENT: Embedding incomplete code comment, should add "and values are their emmbeddings"
+        embeddings: a dictionary where the keys are words and
+    Output:
+        countries: a dictionary with the most likely country and its similarity score
+    """
+    city1_vec = get_word_vector(embeddings, city1)
+    city2_vec = get_word_vector(embeddings, city2)
+    country1_vec = get_word_vector(embeddings, country1)
+
+    intermediate_vector = country1_vec - city1_vec
+    country2_vec_pred = city2_vec + intermediate_vector
+
+    country = get_closest_vector(embeddings, country2_vec_pred, city1, country1, city2)
+    return country
+
+
+
+
+
+
 
 
---
+++ vector_space_models/week3_assignment.py
@@ -0,0 +1,19 @@
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from vector_space_models.utils import get_country
+
+data = pd.read_csv('./data/Files/capitals.txt', sep=' ')
+data.columns = ['city1', 'country1', 'city2', 'country2']
+data.head()
+
+fh = open('./data/Files/word_embeddings_subset.p', 'rb')
+word_embeddings = pickle.load(fh)
+len(word_embeddings)
+
+print("dimension: {}".format(word_embeddings['Spain'].shape[0]))
+country = get_country('Athens', 'Greece', 'Cairo', word_embeddings)
+
+# Plotting the vectors using PCA
