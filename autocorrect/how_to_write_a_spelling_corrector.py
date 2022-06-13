4cb83ea11dcde15861d46ffc14b959252fc210fc
Karvendhan M <karvsmech@gmail.com>
2022-06-13 08:56:30 +0530

    opening a file, split them into words and count their frequency in corpus using Counter function

--- autocorrect/how_to_write_a_spelling_corrector.py
+++ autocorrect/how_to_write_a_spelling_corrector.py
@@ -10,3 +10,10 @@ def words(text):
     '''
     return re.findall(r'\w+', text.lower())
 
+with open('./data/big.txt', 'r') as fh:
+    big_text = fh.read()
+
+fh.close()
+
+WORDS = Counter(words(big_text))
+
