c300e54d6cf3603ada6f44b73ca8e6d74125a6f4
Karvendhan M <karvsmech@gmail.com>
2022-06-16 09:40:38 +0530

    edits - insert, delete, switch, replace for a word

--- autocorrect/utils.py
+++ autocorrect/utils.py
@@ -50,3 +50,77 @@ def get_probs(word_count_dict):
         probs[key] = (value / tot_words)
 
     return probs
+
+
+def delete_letter(word, verbose=False):
+    '''
+    Input:
+        word: the string/word for which you will generate all possible words
+                in the vocabulary which have 1 missing character
+    Output:
+        delete_l: a list of all possible strings obtained by deleting 1 character from word
+    '''
+
+    split_l = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    delete_l = [L + R[1:] for L, R in split_l if R]
+
+    if verbose: print(f"input word {word}, \nsplit_l = {split_l}, \ndelete_l = {delete_l}")
+
+    return delete_l
+
+
+def switch_letter(word, verbose=False):
+    '''
+    Input:
+        word: input string
+     Output:
+        switches: a list of all possible strings with one adjacent charater switched
+    '''
+
+    split_l = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    switch_l = [L + R[1] + R[0] + R[2:] for L, R in split_l if len(R) >= 2]
+
+    if verbose: print(f"Input word = {word} \nsplit_l = {split_l} \nswitch_l = {switch_l}")
+
+    return switch_l
+
+
+def replace_letter(word, verbose=False):
+    '''
+    Input:
+        word: the input string/word
+    Output:
+        replaces: a list of all possible strings where we replaced one letter from the original word.
+    '''
+
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+
+    split_l = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    replace_set = [L + letter + R[1:] for L, R in split_l if R for letter in letters]
+    replace_set = set(filter(lambda x: x != word, replace_set))
+
+    # turn the set back into a list and sort it, for easier viewing
+    replace_l = sorted(list(replace_set))
+
+    if verbose: print(f"Input word = {word} \nsplit_l = {split_l} \nreplace_l {replace_l}")
+
+    return replace_l
+
+
+def insert_letter(word, verbose=False):
+    '''
+    Input:
+        word: the input string/word
+    Output:
+        inserts: a set of all possible strings with one new letter inserted at every offset
+    '''
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    insert_l = []
+    split_l = []
+
+    split_l = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    insert_l = [L + letter + R for L, R in split_l for letter in letters]
+
+    if verbose: print(f"Input word {word} \nsplit_l = {split_l} \ninsert_l = {insert_l}")
+
+    return insert_l
