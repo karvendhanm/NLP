4e1ae77b11582417c2c123e91e4ee4124e07a873
Karvendhan M <karvsmech@gmail.com>
2022-06-16 08:36:06 +0530

    probability for each word in a corpus to occur.

--- autocorrect/C2_W1_assignment.py
+++ autocorrect/C2_W1_assignment.py
@@ -3,9 +3,14 @@
 # Here we will implement models that are 1 and 2 edit distance away.
 # Data preprocessing
 
-import re
-from collections import Counter
 import numpy as np
 import pandas as pd
+
+from autocorrect.utils import process_data, get_count
+
+word_l = process_data('./data/shakespeare.txt')
+word_count_dict = get_count(word_l)
+
+
 
 
