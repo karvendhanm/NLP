fffec5d1d489ac75cc4d2ce4473ac957420d66c5
Karvendhan M <karvsmech@gmail.com>
2022-06-21 09:36:32 +0530

    vectorization using numpy for POS tagging

--- parts_of_speech_tagging/working_with_tags(POS).py
+++ parts_of_speech_tagging/working_with_tags(POS).py
@@ -1,7 +1,18 @@
 # Parts of speech tagging - Working with tags and Numpy
-
+import math
 import numpy as np
 import pandas as pd
+
+
+def print_matrix(_matrix, _tags):
+    '''
+
+    :param _matrix:
+    :param _tags:
+    :return:
+    '''
+    print(pd.DataFrame(_matrix, index=_tags, columns=_tags))
+
 
 # Define tags for Adverb, Noun and To (the preposition) , respectively
 tags = ['RB', 'NN', 'TO']
@@ -27,7 +38,24 @@ sorted_tags = sorted(tags)
 for i in range(num_tags):
     for j in range(num_tags):
         tag_tuple = (sorted_tags[i], sorted_tags[j])
-        transition_matrix[i, j] = transition_counts[tag_tuple]
+        transition_matrix[i, j] = transition_counts.get(tag_tuple)
 
+row_sum = transition_matrix.sum(axis=1, keepdims=True)
+transition_matrix = transition_matrix/10
+transition_matrix = transition_matrix / row_sum
+print_matrix(transition_matrix, sorted_tags)
 
+# Copy transition matrix for numpy functions example
+t_matrix_for = np.copy(transition_matrix)
+t_matrix_np = np.copy(transition_matrix)
 
+for i in range(num_tags):
+    t_matrix_for[i, i] = t_matrix_for[i, i] + math.log(row_sum[i])
+
+print(t_matrix_for)
+
+mat_diagonal = np.diagonal(t_matrix_np).reshape(row_sum.shape)
+mat_diagonal = mat_diagonal + np.vectorize(math.log)(row_sum)
+np.fill_diagonal(t_matrix_np, mat_diagonal)
+
+print(t_matrix_np)
