a789017e8e3c59e066b7404346a3f3abcb9eb160
Karvendhan M <karvsmech@gmail.com>
2022-06-25 21:34:40 +0530

    course 2 week 2 assignment

--- parts_of_speech_tagging/C2_W2_POS_tagging.py
+++ parts_of_speech_tagging/C2_W2_POS_tagging.py
@@ -1,1 +1,10 @@
 import pandas as pd
+from collections import defaultdict
+import math
+import numpy as np
+
+with open('./data/WSJ_02-21.pos', 'r') as fh:
+    data = fh.readlines()
+
+for line in data:
+    print(line)
