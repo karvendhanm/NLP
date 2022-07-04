cb8a8d0937c8e914d95c46170fa134aba5a5e8a2
Karvendhan M <karvsmech@gmail.com>
2022-07-04 20:57:30 +0530

    adding the training and test data set

--- parts_of_speech_tagging/C2_W2_POS_tagging.py
+++ parts_of_speech_tagging/C2_W2_POS_tagging.py
@@ -4,8 +4,14 @@ import math
 import numpy as np
 
 with open('./data/WSJ_02-21.pos', 'r') as fh:
-    data = fh.readlines()
-    for line in data:
-        print(line)
+    training_corpus = fh.readlines()
 
+print(training_corpus[0:5])
+
+with open('./data/hmm_vocab.txt', 'r') as fh:
+    voc_l = fh.read().split('\n')
+
+print(voc_l[0:50])
+print('few items at the end of the vocabulary list')
+print(voc_l[-50:])
 
